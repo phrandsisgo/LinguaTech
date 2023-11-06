ba7cd7249cb49c15952c4ece6826b6a7f2279fcf
Francisco Wohlgemuth <francisco.wohlgemuth@hotmail.com>
2023-11-06 16:41:33 +0100

    An Animationen wurde weiter gearbeitet.

--- resources/views/swipeTest.blade.php
+++ resources/views/swipeTest.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://hammerjs.github.io/dist/hammer.js"></script> 
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
     <!-- hier kommt mein script für hammer.js hin -->
 
     <script>
@@ -13,25 +17,50 @@
             var hammertime = new Hammer(myElement);
             hammertime.on('swipe', function(ev) {
                 console.log(ev);
-                alert(ev.direction);
+               // alert(ev.direction);
                 if(ev.direction === Hammer.DIRECTION_LEFT){
-                    alert("swipe left");
+                triggerAnimationLeft();
+                   // alert("swipe left");
                     handleLeftClick();
                 }else if(ev.direction === Hammer.DIRECTION_RIGHT){
-                    alert("swipe right");
+                triggerAnimationRight();
+                    //alert("swipe right");
                     handleRightClick();
                 }
             });
 
-            function handleLeftClick() {
-                console.log("Links geklickt!");
+            function handleLeftClickLeft() {
+                triggerAnimationLeft();
+                //console.log("Links geklickt!");
                 naechsteKarte();
             }
             function handleRightClick() {
-                console.log("Rechts geklickt!");
+                triggerAnimationRight();
+               // console.log("Rechts geklickt!");
                 naechsteKarte();
          }
         });
+
+        function triggerAnimationRight(){
+            //const karteContent = document.querySelector('.flashCardContent');
+            const karteContent = document.getElementById('flashCardContent');
+            karteContent.classList.add('animate__rollOut__right');
+
+            //  make sure to remove the class after the animation ends to allow it to be triggered again
+            karteContent.addEventListener('animationend', function(){
+                karteContent.classList.remove('animate__rollOut__right');
+            });
+        }
+        function triggerAnimationLeft(){
+            //const karteContent = document.querySelector('.flashCardContent');
+            const karteContent = document.getElementById('flashCardContent');
+            karteContent.classList.add('animate__rollOut__left');
+
+            //  make sure to remove the class after the animation ends to allow it to be triggered again
+            karteContent.addEventListener('animationend', function(){
+                karteContent.classList.remove('animate__rollOut__left');
+            });
+        }
     </script>
 
     <script defer>
@@ -63,7 +92,7 @@
         });
 
         function handleLeftClick() {
-            console.log("Links geklickt!");
+           // console.log("Links geklickt!");
             naechsteKarte();
         }
 
@@ -72,6 +101,9 @@
             naechsteKarte();
         }
         function showUebersetzung(){
+
+            const karteContent = document.getElementById('flashCardContent');
+            karteContent.classList.add('animate__flipOutY');
             const baseLangWord = document.querySelector('.karteInfo');
             const index = baseLangWord.getAttribute("data-index");
             if(baseLangWord.textContent == woerterbuch[index][0]){
@@ -103,6 +135,7 @@
         document.getElementById('zurueckBtn').addEventListener('click', function() {
             document.getElementById('karteModal').style.display = 'none';
         });
+        
 
 
     </script>
@@ -148,23 +181,129 @@
 
             border: 5px solid green;
          }
+            .confirmRight{
+                position: absolute;
+                top: 80%;
+                left:84%;
+            }
+            .confirmLeft{
+                position: absolute;
+                top: 80%;
+                left: 16%;
+            }
+            
+         @-webkit-keyframes rollOutRight {
+            from {
+                opacity: 1;
+            }
+            to {
+                opacity: 0;
+                -webkit-transform: translate3d(100%, 0, 0) rotate3d(0, 0, 1, 120deg);
+                transform: translate3d(100%, 0, 0) rotate3d(0, 0, 1, 120deg);
+            }
+            }
+            @keyframes rollOutRight {
+            from {
+                opacity: 1;
+            }
+            to {
+                opacity: 0;
+                -webkit-transform: translate3d(100%, 0, 0) rotate3d(0, 0, 1, 120deg);
+                transform: translate3d(100%, 0, 0) rotate3d(0, 0, 1, 120deg);
+            }
+            }
+            .animate__rollOut__right {
+            -webkit-animation-name: rollOutRight;
+            animation-name: rollOutRight;
+            /* Define the duration, fill mode, etc. for the animation */
+            -webkit-animation-duration: 0.7s;
+            animation-duration: 0.7s;
+            -webkit-animation-fill-mode: forwards;
+            animation-fill-mode: forwards;
+            }
+
+            @-webkit-keyframes rollOutLeft {
+            from {
+                opacity: 1;
+            }
+            to{
+                opacity: 0;
+                -webkit-transform: translate3d(-100%, 0, 0) rotate3d(0, 0, 1, -120deg);
+                transform: translate3d(-100%, 0, 0) rotate3d(0, 0, 1, -120deg);
+            }}
+            @keyframes rollOutLeft {
+            from {
+                opacity: 1;
+            }
+            to{
+                opacity: 0;
+                -webkit-transform: translate3d(-100%, 0, 0) rotate3d(0, 0, 1, -120deg);
+                transform: translate3d(-100%, 0, 0) rotate3d(0, 0, 1, -120deg);
+            }}
+            .animate__rollOut__left {
+            -webkit-animation-name: rollOutLeft;
+            animation-name: rollOutLeft;
+            /* Define the duration, fill mode, etc. for the animation */
+            -webkit-animation-duration: 0.7s;
+            animation-duration: 0.7s;
+            -webkit-animation-fill-mode: forwards;
+            animation-fill-mode: forwards;
+            }
+
+            /*hier noch die Animation für das flippen der Karten*/
+
+            @keyframes flipOutY {
+            from {
+                -webkit-transform: perspective(400px);
+                transform: perspective(400px);
+                transform-origin: center;
+            }
+
+            30% {
+                -webkit-transform: perspective(400px) rotate3d(0, 1, 0, -15deg);
+                transform: perspective(400px) rotate3d(0, 1, 0, -15deg);
+                opacity: 1;
+            }
+
+            to {
+                -webkit-transform: perspective(400px) rotate3d(0, 1, 0, 90deg);
+                transform: perspective(400px) rotate3d(0, 1, 0, 90deg);
+                opacity: 0;
+            }}
+            .animate__flipOutY {
+            -webkit-animation-duration: calc(1s * 0.75);
+            animation-duration: calc(1s * 0.75);
+            -webkit-animation-duration: calc(var(--animate-duration) * 0.75);
+            animation-duration: calc(var(--animate-duration) * 0.75);
+            -webkit-backface-visibility: visible !important;
+            backface-visibility: visible !important;
+            -webkit-animation-name: flipOutY;
+            animation-name: flipOutY;
+            
+            }
 
     </style>
 </head>
 <body>
-<button id="startPlay" onclick="openModal()">Start Play</button>
+<button id="startPlay" onclick="openModal()" class="animate__flip">Start Play</button>
 
 
     <div id="karteModal" class="modal">
-        <div class="karteContent" id="flashCardContent">
-            <button id="zurueckBtn">Zurück</button>
+        <div class="karteContent animate__flip" id="flashCardContent">   
+            <div class="confirmRight"><img src="https://www.svgrepo.com/show/384403/accept-check-good-mark-ok-tick.svg" alt="accept" height="20px"></div>
+            <div class="confirmLeft">
+                <img src="{{ asset('questionmark-icon.svg')}}" alt="questionmark-icon" height="20">
+            </div>
+            <button id="zurueckBtn" onclick="triggerAnimation()">Zurück</button>
             <div class="kartenInhalt">
-                <div class="karteLeft" onclick="handleLeftClick()">← Links</div>
+                <div class="karteLeft" onclick="handleLeftClick()">← Links </div>
+
                 <div> </div>
                 <span onclick="showUebersetzung()"><div class="karteInfo">kart1</div></span>
                 <div> </div>
                 <div class="karteRight" onclick="handleRightClick()">Rechts →</div>
-            </div>
+            </div> 
+         
     </div>
 
 </body>
