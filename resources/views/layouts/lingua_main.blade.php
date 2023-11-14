9e51c2e57204b90315ff5f08cd6d19aebeb8e5d3
Francisco Wohlgemuth <francisco.wohlgemuth@hotmail.com>
2023-11-14 14:19:22 +0100

    grundgerüst von navbar ist done

--- resources/views/layouts/lingua_main.blade.php
+++ resources/views/layouts/lingua_main.blade.php
@@ -10,18 +10,27 @@
 </head>
 <body>
 <nav>
-    <p>lingua tech</p>
-    <img src="{{ asset('icons/books-icon.svg')}}" alt="book Icon">
-    <img src="{{ asset('icons/home-icon.svg')}}" alt="home Icon">
-    <img src="{{ asset('icons/library-icon.svg')}}" alt="library Icon">
-    <p id="navText">LinguaTech</p>
-    <img src="{{ asset('icons/menu-icon.svg')}}" alt="menu Icon">
-    <img src="{{ asset('icons/pencil-icon.svg')}}" alt="pencil Icon" height="80px" width="80px">
-    <!--  -->
+   <div class="nav-wrapper">
+        <img src="{{ asset('icons/home-icon.svg')}}" alt="home Icon" class="icons">
+        <div class="horizontal-fill"></div>
+        <p id="navText">LinguaTech</p>
+        <div class="horizontal-fill"></div>
+        <img src="{{ asset('icons/info-icon.svg')}}" alt="impressum Icon" class="icons des-only">
+        <img src="{{ asset('icons/library-icon.svg')}}" alt="library Icon" class="icons des-only">
+        <img src="{{ asset('icons/menu-icon.svg')}}" alt="menu Icon" class="icons">
+        <!--  -->
+   </div> 
 </nav>
 @yield('content')
 <footer>
-    <p>lingua tech</p>
+    <div class="footer_box">
+        <p>lingua tech</p>
+        <div class="horizontal-fill"></div>
+        <img src="{{ asset('icons/info-icon.svg')}}" alt="impressum Icon" class="icons">
+        <div class="horizontal-fill"></div>
+        <img src="{{ asset('icons/library-icon.svg')}}" alt="library Icon" class="icons ">
+        <div class="horizontal-fill"></div>
+    </div>
 </footer>
 </body>
 </html>
