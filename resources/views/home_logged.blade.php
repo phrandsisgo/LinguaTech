3a1d007cc129382f68d87dc7dac7770fb89aa2ea
Francisco Wohlgemuth <francisco.wohlgemuth@hotmail.com>
2023-11-14 16:59:10 +0100

    First designs are done.

--- resources/views/home_logged.blade.php
+++ resources/views/home_logged.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.lingua_main')
 @section('title', 'Home')
 @section('content')
-    <h1>Willkommen</h1>
-    <p>Bei Lingua Tech</p>
+    
+<p class="pagetitle">Über mich</p>
+<img src="{{ asset('profilbild.jpg')}}" alt="" id="profilBild">
+<p class="section-content">Hallo zusammen, mein Name ist Francisco Wohlgemuth. Ich bin angehender Web-developer. Auf dieser Webseite versuche ich, eine Sprach-App zu bauen, da ich mit anderen Sprach-Apps nicht zufrieden war." Ursprünglich stamme ich aus der Baubranche, wo ich als Elektroinstallateur tätig war. Doch dann entwickelte sich meine Leidenschaft für die Webentwicklung. Ich lerne gerne neue Verfahren über das programmieren und lerne gerne immer mehr dazu. Zur zeil liegt mein Fokus voll und ganz auf die Web-Entwicklung und ich bin zur Zeit auf der Stellensuche als web-entwickler in einer Junior position. Schauen Sie sich gerne meine Projekte auf meinem GitHub-Profil an.</p>
 @endsection
--- resources/views/layouts/lingua_main.blade.php
+++ resources/views/layouts/lingua_main.blade.php
@@ -21,16 +21,23 @@
         <!--  -->
    </div> 
 </nav>
+<div class="mainContent">
 @yield('content')
+</div>
+<div class="heightbox"></div>
 <footer>
-    <div class="footer_box">
-        <p>lingua tech</p>
-        <div class="horizontal-fill"></div>
-        <img src="{{ asset('icons/info-icon.svg')}}" alt="impressum Icon" class="icons">
-        <div class="horizontal-fill"></div>
-        <img src="{{ asset('icons/library-icon.svg')}}" alt="library Icon" class="icons ">
-        <div class="horizontal-fill"></div>
+    <div class="footer-Wrapper">
+        <div class="gradiant-box"></div>
+        <div class="footer_box">
+            <p>lingua tech</p>
+            <div class="horizontal-fill"></div>
+            <img src="{{ asset('icons/info-icon.svg')}}" alt="impressum Icon" class="icons">
+            <div class="horizontal-fill"></div>
+            <img src="{{ asset('icons/library-icon.svg')}}" alt="library Icon" class="icons ">
+            <div class="horizontal-fill"></div>
+        </div>
     </div>
 </footer>
+
 </body>
 </html>
