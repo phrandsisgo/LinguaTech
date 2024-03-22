95526b088c5af51523135cec7689725423da5f63
Francisco <francisco.wohlgemuth@hotmail.com>
2024-03-22 13:11:32 +0100

    SetLanguageMiddleware is now fixed

--- app/Http/Middleware/SetLanguageMiddleware.php
+++ app/Http/Middleware/SetLanguageMiddleware.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
 
 class SetLanguageMiddleware
 {
@@ -15,6 +17,9 @@ class SetLanguageMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if (Session::has('locale')) {
+            App::setLocale(Session::get('locale'));
+        }
         return $next($request);
     }
 }
